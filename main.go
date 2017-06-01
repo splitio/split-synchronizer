a118dd0e7e080ffb3b15b3541c0ea18c29f3fc6b
Sebastian Arrubia <sarrubia@gmail.com>
2017-06-01 18:47:52 -0300

    modify name

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ var cliParametersMap map[string]interface{}
 func init() {
 	//Show initial banner
 	fmt.Println(splitio.ASCILogo)
-	fmt.Println("Split Software Agent - Version: ", splitio.Version)
+	fmt.Println("Split Synchroniser Agent - Version: ", splitio.Version)
 
 	//reading command line options
 	parseFlags()
