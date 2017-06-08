3c3730033d47ab99f7fd3a4b3018194426cca1d8
Sebastian Arrubia <sarrubia@gmail.com>
2017-06-08 15:48:23 -0300

    Adding commit version at installer

--- release/build.sh
+++ release/build.sh
@@ -21,18 +21,20 @@ zip -9 ${OSX_ZIP_FILENAME}  ${OSX_BINARY_PATH}
 zip -9 ${LINUX_ZIP_FILENAME} ${LINUX_BINARY_PATH}
 zip -9 ${WINDOWS_ZIP_FILENAME} ${WINDOWS_BINARY_PATH}
 
+COMMIT_VERSION=`git rev-parse --short HEAD`
+
 TEMPLATELINES=`wc -l install_script_template | awk '{print $1}'`
 TOTALLINES=$(($TEMPLATELINES + 1))
 
 OSX_SUM=`sum ${OSX_ZIP_FILENAME}`
 OSX_ASUM1=`echo "${OSX_SUM}" | awk '{print $1}'`
 OSX_ASUM2=`echo "${OSX_SUM}" | awk '{print $2}'`
-cat install_script_template | sed -e "s/AUTO_REPLACE_SCRIPT_LINES/${TOTALLINES}/" | sed -e "s/AUTO_REPLACE_BIN_FILENAME/${OSX_BINARY_PATH}/" | sed -e "s/AUTO_REPLACE_SUM1/${OSX_ASUM1}/" | sed -e "s/AUTO_REPLACE_SUM2/${OSX_ASUM2}/" > ${OSX_INSTALL_SCRIPT}
+cat install_script_template | sed -e "s/AUTO_REPLACE_COMMIT_VERSION/${COMMIT_VERSION}/" | sed -e "s/AUTO_REPLACE_SCRIPT_LINES/${TOTALLINES}/" | sed -e "s/AUTO_REPLACE_BIN_FILENAME/${OSX_BINARY_PATH}/" | sed -e "s/AUTO_REPLACE_SUM1/${OSX_ASUM1}/" | sed -e "s/AUTO_REPLACE_SUM2/${OSX_ASUM2}/" > ${OSX_INSTALL_SCRIPT}
 
 LINUX_SUM=`sum ${LINUX_ZIP_FILENAME}`
 LINUX_ASUM1=`echo "${LINUX_SUM}" | awk '{print $1}'`
 LINUX_ASUM2=`echo "${LINUX_SUM}" | awk '{print $2}'`
-cat install_script_template | sed -e "s/AUTO_REPLACE_SCRIPT_LINES/${TOTALLINES}/" | sed -e "s/AUTO_REPLACE_BIN_FILENAME/${LINUX_BINARY_PATH}/" | sed -e "s/AUTO_REPLACE_SUM1/${LINUX_ASUM1}/" | sed -e "s/AUTO_REPLACE_SUM2/${LINUX_ASUM2}/" > ${LINUX_INSTALL_SCRIPT}
+cat install_script_template | sed -e "s/AUTO_REPLACE_COMMIT_VERSION/${COMMIT_VERSION}/" | sed -e "s/AUTO_REPLACE_SCRIPT_LINES/${TOTALLINES}/" | sed -e "s/AUTO_REPLACE_BIN_FILENAME/${LINUX_BINARY_PATH}/" | sed -e "s/AUTO_REPLACE_SUM1/${LINUX_ASUM1}/" | sed -e "s/AUTO_REPLACE_SUM2/${LINUX_ASUM2}/" > ${LINUX_INSTALL_SCRIPT}
 
 #Create installers
 make
