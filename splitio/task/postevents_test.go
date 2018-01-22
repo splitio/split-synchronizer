1ef07738db76b6ca937e0b73775246511a4c3ab6
Sebastian Arrubia <sarrubia@gmail.com>
2018-01-22 16:55:56 -0300

    Events sync task unit test

---
+++ splitio/task/postevents_test.go
@@ -0,0 +1,130 @@
+package task
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/splitio/split-synchronizer/conf"
+	"github.com/splitio/split-synchronizer/log"
+	"github.com/splitio/split-synchronizer/splitio/api"
+	"github.com/splitio/split-synchronizer/splitio/recorder"
+	"github.com/splitio/split-synchronizer/splitio/storage/redis"
+)
+
+func TestTaskPostEvents(t *testing.T) {
+
+	stdoutWriter := ioutil.Discard //os.Stdout
+	log.Initialize(stdoutWriter, stdoutWriter, stdoutWriter, stdoutWriter, stdoutWriter, stdoutWriter)
+
+	bulkSize := 10
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		sdkVersion := r.Header.Get("SplitSDKVersion")
+		sdkMachine := r.Header.Get("SplitSDKMachineIP")
+		sdkMachineName := r.Header.Get("SplitSDKMachineName")
+
+		if sdkVersion != "test-1.0.0" {
+			t.Error("SDK Version HEADER not match")
+		}
+
+		if sdkMachine != "127.0.0.1" {
+			t.Error("SDK Machine HEADER not match")
+		}
+
+		if sdkMachineName != "SOME_MACHINE_NAME" {
+			t.Error("SDK Machine Name HEADER not match")
+		}
+
+		rBody, _ := ioutil.ReadAll(r.Body)
+		//fmt.Println(string(rBody))
+
+		var eventsInPost []api.EventDTO
+		err := json.Unmarshal(rBody, &eventsInPost)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		if len(eventsInPost) != bulkSize {
+			t.Error("Invalid amount of events")
+		}
+
+		if eventsInPost[0].EventTypeID != "a5213963-5564-43ff-83b2-ac6dbd5af3b1" {
+			t.Error("Invalid EventTypeID")
+		}
+
+		if eventsInPost[0].Key != "6c4829ab-a0d8-4e72-8176-a334f596fb79" {
+			t.Error("Invalid KEY")
+		}
+
+		if eventsInPost[0].Timestamp != 1516310749882 {
+			t.Error("Invalid Timestamp")
+		}
+
+		if eventsInPost[0].TrafficTypeName != "user" {
+			t.Error("Invalid TrafficTypeName")
+		}
+
+		if eventsInPost[0].Value != 2993.4876 {
+			t.Error("Invalid Value")
+		}
+
+	}))
+
+	defer ts.Close()
+
+	os.Setenv("SPLITIO_SDK_URL", ts.URL)
+	os.Setenv("SPLITIO_EVENTS_URL", ts.URL)
+
+	// API initilization
+	api.Initialize()
+
+	//Initialize by default
+	conf.Initialize()
+
+	conf.Data.Redis.Prefix = "posteventunittest"
+
+	//Redis storage by default
+	redis.Initialize(conf.Data.Redis)
+
+	//INSERT MOCK DATA
+	//----------------
+	itemsToAdd := 50
+	eventListName := conf.Data.Redis.Prefix + ".SPLITIO.events"
+
+	eventJSON := `{"m":{"s":"test-1.0.0","i":"127.0.0.1","n":"SOME_MACHINE_NAME"},"e":{"key":"6c4829ab-a0d8-4e72-8176-a334f596fb79","trafficTypeName":"user","eventTypeId":"a5213963-5564-43ff-83b2-ac6dbd5af3b1","value":2993.4876,"timestamp":1516310749882}}`
+
+	//Deleting previous test data
+	res := redis.Client.Del(eventListName)
+	if res.Err() != nil {
+		t.Error(res.Err().Error())
+		return
+	}
+
+	//Pushing 50 events
+	for i := 0; i < itemsToAdd; i++ {
+		redis.Client.RPush(eventListName, eventJSON)
+	}
+	//----------------
+
+	tid := 1
+	eventsRecorderAdapter := recorder.EventsHTTPRecorder{}
+	eventsStorageAdapter := redis.NewEventStorageAdapter(redis.Client, conf.Data.Redis.Prefix)
+	//Catching panic status and reporting error
+	func() {
+		defer func() {
+			if r := recover(); r != nil {
+				t.Error("Recovered task", r)
+			}
+		}()
+		taskPostEvents(tid, eventsRecorderAdapter, eventsStorageAdapter, int64(bulkSize))
+	}()
+
+	time.Sleep(10 * time.Second)
+}
