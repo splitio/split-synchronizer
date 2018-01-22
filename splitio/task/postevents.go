743c279d0627d4a9e4793aed149ca205594ca1a8
Sebastian Arrubia <sarrubia@gmail.com>
2018-01-22 16:55:38 -0300

    Events sync task

--- splitio/task/postevents.go
+++ splitio/task/postevents.go
@@ -18,7 +18,7 @@ func taskPostEvents(tid int,
 	//[SDKVersion][MachineIP][MachineName]
 	toSend := make(map[string]map[string]map[string][]api.EventDTO)
 
-	storedEvents, err := storageAdapter.PopN(bulkSize)
+	storedEvents, err := storageAdapter.PopN(bulkSize) //PopN has a mutex, so this function can be async without issues
 	if err != nil {
 		log.Error.Println("(Task) Post Events fails fetching events from storage", err.Error())
 		return
@@ -55,6 +55,16 @@ func taskPostEvents(tid int,
 	}
 
 	// TODO check to send data and posted to server
+	for s, byIP := range toSend {
+		for i, byName := range byIP {
+			for n, bulk := range byName {
+				err := recorderAdapter.Post(bulk, s, i, n)
+				if err != nil {
+					log.Error.Println("Error posting events", err)
+				}
+			}
+		}
+	}
 }
 
 // PostEvents post events to Split Server task
@@ -67,7 +77,7 @@ func PostEvents(
 ) {
 
 	for {
-
+		taskPostEvents(tid, eventsRecorderAdapter, eventsStorageAdapter, int64(eventsBulkSize))
 		time.Sleep(time.Duration(eventsRefreshRate) * time.Second)
 	}
 
