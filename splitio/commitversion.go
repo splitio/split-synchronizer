864f004bc64ea374bbff201cac2efec5d8c88d70
Sebastian Arrubia <sarrubia@gmail.com>
2018-04-20 11:18:03 -0300

    Commit vesrion

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,4 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-const CommitVersion = "31e9673"
+const CommitVersion = "466ce13"
