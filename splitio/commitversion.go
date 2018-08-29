b2758dab3bf2fb7b4eda32d1f5f00c5d8068c4bf
Martin Redolatti <martinredolatti@gmail.com>
2018-08-29 15:43:31 -0300

    update changes.txt

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,4 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-const CommitVersion = "bcc9ae8"
+const CommitVersion = "e3f5c0f"
