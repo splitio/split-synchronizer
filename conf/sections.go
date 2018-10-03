290b8c94532a4e64760ac884b0b07f2c6c94a0ff
Martin Redolatti <martinredolatti@gmail.com>
2018-10-03 16:46:26 -0300

    update defaults, add LUA script to fetch impressions with count, fix malformed keys bug

--- conf/sections.go
+++ conf/sections.go
@@ -64,8 +64,8 @@ type ConfigData struct {
 	ImpressionListener     ImpressionListener `json:"impressionListener" split-cli-option-group:"true"`
 	SplitsFetchRate        int                `json:"splitsRefreshRate" split-cli-option:"split-refresh-rate" split-default-value:"60" split-cli-description:"Refresh rate of splits fetcher"`
 	SegmentFetchRate       int                `json:"segmentsRefreshRate" split-default-value:"60" split-cli-option:"segment-refresh-rate" split-cli-description:"Refresh rate of segments fetcher"`
-	ImpressionsPostRate    int                `json:"impressionsRefreshRate" split-default-value:"60" split-cli-option:"impressions-post-rate" split-cli-description:"Post rate of impressions recorder"`
-	ImpressionsPerPost     int64              `json:"impressionsPerPost" split-cli-option:"impressions-per-post" split-default-value:"1000" split-cli-description:"Number of impressions to send in a POST request"`
+	ImpressionsPostRate    int                `json:"impressionsRefreshRate" split-default-value:"20" split-cli-option:"impressions-post-rate" split-cli-description:"Post rate of impressions recorder"`
+	ImpressionsPerPost     int64              `json:"impressionsPerPost" split-cli-option:"impressions-per-post" split-default-value:"50000" split-cli-description:"Number of impressions to send in a POST request"`
 	ImpressionsThreads     int                `json:"impressionsThreads" split-default-value:"1" split-cli-option:"impressions-recorder-threads" split-cli-description:"Number of impressions recorder threads"`
 	EventsPushRate         int                `json:"eventsPushRate" split-default-value:"60" split-cli-option:"events-push-rate" split-cli-description:"Post rate of event recorder (seconds)"`
 	EventsConsumerReadSize int                `json:"eventsConsumerReadSize" split-default-value:"10000" split-cli-option:"events-consumer-read-size" split-cli-description:"Events queue read size"`
--- splitio/storage/redis/impression_test.go
+++ splitio/storage/redis/impression_test.go
@@ -2,8 +2,10 @@
 package redis
 
 import (
-	"errors"
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"strings"
 	"testing"
 
 	"github.com/splitio/split-synchronizer/conf"
@@ -13,16 +15,16 @@ import (
 
 func findImpressionsForFeature(
 	bulk map[string]map[string][]api.ImpressionsDTO,
-	featureName string,
-	instanceID string,
 	sdk string,
+	instanceID string,
+	featureName string,
 ) (*api.ImpressionsDTO, error) {
 	for _, feature := range bulk[sdk][instanceID] {
 		if feature.TestName == featureName {
 			return &feature, nil
 		}
 	}
-	return nil, errors.New("featre not found")
+	return nil, fmt.Errorf("Feature %s not found", featureName)
 }
 
 func TestImpressionStorageAdapter(t *testing.T) {
@@ -36,7 +38,6 @@ func TestImpressionStorageAdapter(t *testing.T) {
 	languageAndVersion := "test-2.0"
 	instanceID := "127.0.0.1"
 	featureName := "some_feature"
-
 	impressionTXT1 := `{"keyName":"some_key1","treatment":"off","time":1234567890,"changeNumber":55555555,"label":"some label","bucketingKey":"some_bucket_key"}`
 	impressionTXT2 := `{"keyName":"some_key2","treatment":"on","time":1234567999,"changeNumber":577775,"label":"some label no match","bucketingKey":"some_bucket_key_2"}`
 
@@ -71,7 +72,6 @@ func TestImpressionStorageAdapter(t *testing.T) {
 	if len(impressionDTO.KeyImpressions) != 2 {
 		t.Error("Error counting fetched impressions")
 	}
-
 }
 
 func TestThatQuotaiIsApplied(t *testing.T) {
@@ -84,7 +84,6 @@ func TestThatQuotaiIsApplied(t *testing.T) {
 
 	languageAndVersion := "test-2.0"
 	instanceID := "127.0.0.1"
-	featureName := "some_feature"
 
 	impressionsRaw := map[string][]api.ImpressionDTO{
 		"feature1": {
@@ -127,6 +126,14 @@ func TestThatQuotaiIsApplied(t *testing.T) {
 				Label:        "label1",
 				Time:         123,
 				Treatment:    "treatment5",
+			},
+			{
+				BucketingKey: "",
+				ChangeNumber: 0,
+				KeyName:      "key6",
+				Label:        "label1",
+				Time:         123,
+				Treatment:    "treatment6",
 			},
 		},
 		"feature2": {
@@ -184,17 +191,16 @@ func TestThatQuotaiIsApplied(t *testing.T) {
 	}
 
 	prefixAdapter := &prefixAdapter{prefix: ""}
-
 	//Adding impressions to retrieve.
 	for feature, impressions := range impressionsRaw {
 		for _, impression := range impressions {
+			toStore, _ := json.Marshal(impression)
 			Client.SAdd(
-				prefixAdapter.impressionsNamespace(languageAndVersion, instanceID, featureName),
-				impression,
+				prefixAdapter.impressionsNamespace(languageAndVersion, instanceID, feature),
+				toStore,
 			)
 		}
 	}
-
 	conf.Data.ImpressionsPerPost = 9
 	impressionsStorageAdapter := NewImpressionStorageAdapter(Client, "")
 	retrievedImpressions, err := impressionsStorageAdapter.RetrieveImpressions()
@@ -205,19 +211,185 @@ func TestThatQuotaiIsApplied(t *testing.T) {
 
 	// We set an impressionsPerPost value of 6. Which means that when distributed evenly, we should get 3 impressions
 	// per feature. Since feature 2 has only one, we should get 3 for feature1, 1 for feature2 and 5 for feature3.
-	feature1Impressions, _ := findImpressionsForFeature(retrievedImpressions, languageAndVersion, instanceID, "feature1")
+	feature1Impressions, err := findImpressionsForFeature(retrievedImpressions, languageAndVersion, instanceID, "feature1")
+	if err != nil {
+		t.Error(err.Error())
+		return
+	}
 	if len(feature1Impressions.KeyImpressions) != 3 {
-		t.Error("We should have 3 impressions for feature 1")
+		t.Errorf("We should have 3 impressions for feature1, we have %d", len(feature1Impressions.KeyImpressions))
 	}
 
 	feature2Impressions, _ := findImpressionsForFeature(retrievedImpressions, languageAndVersion, instanceID, "feature2")
-	if len(feature2Impressions.KeyImpressions) != 2 {
-		t.Error("We should have 1 impressions for feature 2")
+	if err != nil {
+		t.Error(err.Error())
+		return
+	}
+	if len(feature2Impressions.KeyImpressions) != 1 {
+		t.Errorf("We should have 3 impressions for feature2, we have %d", len(feature2Impressions.KeyImpressions))
 	}
 
 	feature3Impressions, _ := findImpressionsForFeature(retrievedImpressions, languageAndVersion, instanceID, "feature3")
+	if err != nil {
+		t.Error(err.Error())
+		return
+	}
 	if len(feature3Impressions.KeyImpressions) != 5 {
-		t.Error("We should have 5 impressions for feature 3")
+		t.Errorf("We should have 5 impressions for feature3, we have %d", len(feature3Impressions.KeyImpressions))
+	}
+}
+
+func TestLuaScriptFailure(t *testing.T) {
+	stdoutWriter := ioutil.Discard //os.Stdout
+	log.Initialize(stdoutWriter, stdoutWriter, stdoutWriter, stdoutWriter, stdoutWriter, stdoutWriter)
+
+	//Initialize by default
+	conf.Initialize()
+	Initialize(conf.Data.Redis)
+
+	impressionKeysWithCardinalityScriptTemplate = `local impkeys = redis.call('KEYS', '{KEY_NAME`
+
+	impressionsStorageAdapter := NewImpressionStorageAdapter(Client, "")
+	imps, err := impressionsStorageAdapter.getImpressionsWithCardinality()
+	if imps != nil {
+		t.Error("Resulting impressions should be nil")
+		return
 	}
 
+	if err == nil {
+		t.Error("Should have failed")
+		return
+	}
+
+	if !strings.Contains(
+		err.Error(),
+		"Failed to execute LUA script: ERR Error compiling script (new function): user_script:1: unfinished string near ''{KEY_NAME'",
+	) {
+		t.Error("Incorrect error cause")
+		t.Error(err.Error())
+		return
+	}
+
+	imps2, err := impressionsStorageAdapter.RetrieveImpressions()
+	if imps2 == nil {
+		t.Error("Impressions should not be null, fallback should work correctly")
+		return
+	}
+
+	if err != nil {
+		t.Error("No error should have been generated. fallback should work correctly")
+		return
+	}
+}
+
+func TestLuaScriptReturnsIncorrectType(t *testing.T) {
+	stdoutWriter := ioutil.Discard //os.Stdout
+	log.Initialize(stdoutWriter, stdoutWriter, stdoutWriter, stdoutWriter, stdoutWriter, stdoutWriter)
+
+	//Initialize by default
+	conf.Initialize()
+	Initialize(conf.Data.Redis)
+
+	impressionKeysWithCardinalityScriptTemplate = `return 1`
+
+	impressionsStorageAdapter := NewImpressionStorageAdapter(Client, "")
+	imps, err := impressionsStorageAdapter.getImpressionsWithCardinality()
+	if imps != nil {
+		t.Error("Resulting impressions should be nil")
+		return
+	}
+
+	if err == nil {
+		t.Error("Should have failed")
+		return
+	}
+
+	if !strings.Contains(
+		err.Error(),
+		"Failed to type-assert script's output. []interface {}",
+	) {
+		t.Error("Incorrect error cause")
+		t.Error(err.Error())
+		return
+	}
+
+	imps2, err := impressionsStorageAdapter.RetrieveImpressions()
+	if imps2 == nil {
+		t.Error("Impressions should not be null, fallback should work correctly")
+		return
+	}
+
+	if err != nil {
+		t.Error("No error should have been generated. fallback should work correctly")
+		return
+	}
+}
+
+func TestLuaScriptReturnsIncorrectSlice(t *testing.T) {
+	stdoutWriter := ioutil.Discard //os.Stdout
+	log.Initialize(stdoutWriter, stdoutWriter, stdoutWriter, stdoutWriter, stdoutWriter, stdoutWriter)
+
+	//Initialize by default
+	conf.Initialize()
+	Initialize(conf.Data.Redis)
+
+	impressionKeysWithCardinalityScriptTemplate = `return {1, 2, 3}`
+
+	impressionsStorageAdapter := NewImpressionStorageAdapter(Client, "")
+	imps, err := impressionsStorageAdapter.getImpressionsWithCardinality()
+	if imps != nil {
+		t.Error("Resulting impressions should be nil")
+		return
+	}
+
+	if err == nil {
+		t.Error("Should have failed")
+		return
+	}
+
+	if !strings.Contains(
+		err.Error(),
+		"Failed to type-assert returned structure: Error casting 1 to string, it's int64",
+	) {
+		t.Error("Incorrect error cause")
+		t.Error(err.Error())
+		return
+	}
+
+	imps2, err := impressionsStorageAdapter.RetrieveImpressions()
+	if imps2 == nil {
+		t.Error("Impressions should not be null, fallback should work correctly")
+		return
+	}
+
+	if err != nil {
+		t.Error("No error should have been generated. fallback should work correctly")
+		return
+	}
+}
+
+func TestThatMalformedImpressionKeysDoNotPanic(t *testing.T) {
+	wrongKeys := []string{
+		"SPLITIO/php-5.3.1//impressions.mono_signal_unique_payers_for_payee",
+		"SPLITIO/php-5.3.1///impressions.mono_signal_unique_payers_for_payee",
+		"SPLITIO//ip-123-123-123-123/impressions.mono_signal_unique_payers_for_payee",
+		"SPLITIO///impressions.mono_signal_unique_payers_for_payee",
+		"SPLITIO///ip-123-123-123-123//php-5.3.1//impressions.mono_signal_unique_payers_for_payee",
+	}
+
+	for _, key := range wrongKeys {
+		sdk, ip, feature, err := parseImpressionKey(key)
+		if err == nil {
+			t.Error("An error should have been returned.")
+		}
+		if sdk != nil {
+			t.Errorf("Sdk should be nil. Is %s", *sdk)
+		}
+		if ip != nil {
+			t.Errorf("Ip should be nil. Is %s", *ip)
+		}
+		if feature != nil {
+			t.Errorf("Feature should be nil. Is %s", *feature)
+		}
+	}
 }
