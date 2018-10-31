9790b72ad13ab68ce334730c0900a91ff1944259
Matias Melograno <matiasmelograno@split.io>
2018-10-31 16:14:40 -0300

    updated dockerfile to execute dep ensure

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,4 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-const CommitVersion = "4ffbc7d"
+const CommitVersion = "7d35bf5"
