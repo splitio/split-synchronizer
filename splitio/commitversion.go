f8a00554cc14dbc319694320877c13d7e392fd02
Matias Melograno <matiasmelograno@split.io>
2018-11-14 10:18:33 -0300

    updated CHANGES for releasing

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,4 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-const CommitVersion = "dc86a3e"
+const CommitVersion = "d84fd70"
