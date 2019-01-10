a9647638716e04279cadd604eced46b066b024d9
Matias Melograno <matiasmelograno@split.io>
2019-01-10 16:00:34 -0300

    fixed tests

--- splitio/web/admin/controllers/controllers_test.go
+++ splitio/web/admin/controllers/controllers_test.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/splitio/split-synchronizer/appcontext"
 	"github.com/splitio/split-synchronizer/conf"
 	"github.com/splitio/split-synchronizer/log"
 	"github.com/splitio/split-synchronizer/splitio/storage/redis"
@@ -106,6 +107,49 @@ func TestGetConfigurationSimple(t *testing.T) {
 
 	if data["proxy"] != nil {
 		t.Error("Should not have config")
+	}
+
+	server.Shutdown(ctx)
+}
+
+func TestGetConfigurationProxyMode(t *testing.T) {
+	stdoutWriter := ioutil.Discard //os.Stdout
+	log.Initialize(stdoutWriter, stdoutWriter, stdoutWriter, stdoutWriter, stdoutWriter, stdoutWriter)
+
+	appcontext.Initialize(appcontext.ProxyMode)
+
+	router := gin.Default()
+	router.GET("/test", func(c *gin.Context) {
+		GetConfiguration(c)
+	})
+
+	server := &http.Server{
+		Addr:    ":9999",
+		Handler: router,
+	}
+
+	go server.ListenAndServe()
+	time.Sleep(3 * time.Second)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	res, _ := http.Get("http://localhost:9999/test")
+	responseBody, _ := ioutil.ReadAll(res.Body)
+
+	var data map[string]interface{}
+	_ = json.Unmarshal([]byte(responseBody), &data)
+
+	if data["mode"] != "ProxyMode" {
+		t.Error("It should be ProxyMode")
+	}
+
+	if data["redis"] != nil {
+		t.Error("Should not have config")
+	}
+
+	if data["proxy"] == nil {
+		t.Error("Should have config")
 	}
 
 	server.Shutdown(ctx)
