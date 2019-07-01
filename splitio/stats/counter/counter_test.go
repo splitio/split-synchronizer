c37418d2785972ff53077c45740c415df7c6e479
Matias Melograno <matiasmelograno@split.io>
2019-07-01 14:59:46 -0300

    increased timeout for tests

--- splitio/stats/counter/counter_test.go
+++ splitio/stats/counter/counter_test.go
@@ -102,6 +102,6 @@ func TestCounter(t *testing.T) {
 	}
 
 	//Delaying test to let PostCounterWorker timeout do its work!
-	time.Sleep(time.Duration(10) * time.Second)
+	time.Sleep(time.Duration(20) * time.Second)
 
 }
