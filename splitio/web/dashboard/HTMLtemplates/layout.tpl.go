fcff4c9970a9aa1886aa961ade502d7e72bf142b
Matias Melograno <matiasmelograno@split.io>
2019-07-31 13:57:36 -0300

    added banner for eviction

--- splitio/web/dashboard/HTMLtemplates/layout.tpl.go
+++ splitio/web/dashboard/HTMLtemplates/layout.tpl.go
@@ -515,6 +515,24 @@ var LayoutTPL = `
               </div>
             </div>
           </div>
+          </br>
+          </br>
+          </br>
+          <div class="alert alert-info" role="alert">
+            <h3 class="alert-heading">Delta Eviction Calculation</h3>
+            <p>Delta calculation measures the capacity of the Synchronizer to process and fine tuning it if the default settings are not sufficient.</p>
+            <hr>
+            <ul>
+              <li>
+                <p class="mb-0">If <span style="font-weight:bold;">ℷ >= 1 (delta)</span>: the current configuration is processing Events or Impressions without keeping elements in the stack. In other words, eviction rate >= generation rate. Split Synchronizer is able flush data as it arrives to the system from the SDKs.</p>
+              </li>
+              <li>
+                <p class="mb-0">If <span style="font-weight:bold;">ℷ < 1 (delta)</span>: the current configuration may not be enough to process all the data coming in, and over time it may produce an always-increasing memory footprint. Recommendation: increase the number of threads or reduce the frequency for evicting elements. We recommend increasing the number of threads if they are still using the default value of 1, and to not exceed the number of cores. On the other hand, when reducing the frequency of element eviction (flush operation), decrease the value in a conservative manner by increments of ten or twenty percent each time.</p>
+              </li>
+            </ul>
+            <p>For further information you can visit <a href="https://help.split.io/hc/en-us/articles/360018343391-Split-Synchronizer-Runbook" class="alert-link">Split Synchronizer Runbook</a>.
+            
+          </div>
         </div>
     {{end}}
         <!-- BACKEND STATS -->
