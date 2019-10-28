992bcf071a70f186b48eef0ee69dbd3a97bcc01a
Matias Melograno <matiasmelograno@split.io>
2019-10-28 19:01:14 -0300

    added support for beacon API for events and impressions when Sync runs in proxy mode

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,4 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-const CommitVersion = "25937c3"
+const CommitVersion = "d22f44d"
