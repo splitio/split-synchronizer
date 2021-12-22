d8c83db9e28e671c4d812735a6c7b8eb4792e8f1
Cristian Daniel Fadón <cristian.fadon@split.io>
2021-12-22 17:57:25 -0300

    Create entrypoints inside docker

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,4 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-const CommitVersion = "e9ef8bc"
+const CommitVersion = "9468071"
