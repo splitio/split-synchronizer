84040030481f96a3135b8e404778fdc7e11c6eaa
Martin Redolatti <martin.redolatti@split.io>
2021-12-22 12:09:23 -0300

    bump OS versions, lib versions & more healtcheck tests

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,4 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-const CommitVersion = "d36598f"
+const CommitVersion = "9c76bfd"
