5cd048c38bf5073dc98c8874b352a4f7fe588476
Martin Redolatti <martin.redolatti@split.io>
2022-04-20 16:24:32 -0300

    proper error returning

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,4 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-const CommitVersion = "3c1e28c"
+const CommitVersion = "74d2c95"
