74d2c9583b45e4635f822817e882f64fdb6cbde4
Martin Redolatti <martin.redolatti@split.io>
2022-04-20 16:14:55 -0300

    update telemetry to fetch multiple keys

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,4 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-const CommitVersion = "eb1abc1"
+const CommitVersion = "3c1e28c"
