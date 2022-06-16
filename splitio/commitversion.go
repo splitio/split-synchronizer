b6926e31310da38eb6e67ad9939145fc5dc58eb1
Matias Melograno <matias.melograno@split.io>
2022-06-16 09:57:13 -0300

    updated docker image

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,4 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-const CommitVersion = "5c57853"
+const CommitVersion = "8c5bc98"
