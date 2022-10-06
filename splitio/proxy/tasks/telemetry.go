306a674264961b99a90c37aeeabb7a2be449675d
Matias Melograno <matias.melograno@split.io>
2022-10-06 16:09:42 -0300

    added beacons apis and telemetry stats for new endpoints

--- splitio/proxy/tasks/telemetry.go
+++ splitio/proxy/tasks/telemetry.go
@@ -102,3 +102,95 @@ func newTelemetryUsageWorkerFactory(name string, recorder *api.HTTPTelemetryReco
 func NewTelemetryUsageFlushTask(recorder *api.HTTPTelemetryRecorder, logger logging.LoggerInterface, period int, queueSize int, threads int) *DeferredRecordingTaskImpl {
 	return newDeferredFlushTask(logger, newTelemetryUsageWorkerFactory("telemetry-config-worker", recorder, logger), period, queueSize, threads)
 }
+
+// Keys Client Side
+
+// TelemetryKeysClientSideWorker defines a component capable of recording mtk client side in raw form
+type TelemetryKeysClientSideWorker struct {
+	name     string
+	logger   logging.LoggerInterface
+	recorder *api.HTTPTelemetryRecorder
+}
+
+// Name returns the name of the worker
+func (w *TelemetryKeysClientSideWorker) Name() string { return w.name }
+
+// OnError is called whenever theres an error in the worker function
+func (w *TelemetryKeysClientSideWorker) OnError(e error) {}
+
+// Cleanup is called after the worker is shutdown
+func (w *TelemetryKeysClientSideWorker) Cleanup() error { return nil }
+
+// FailureTime specifies how long to wait when an errors occurs before executing again
+func (w *TelemetryKeysClientSideWorker) FailureTime() int64 { return 1 }
+
+// DoWork is called and passed a message fetched from the work queue
+func (w *TelemetryKeysClientSideWorker) DoWork(message interface{}) error {
+	asTelemetryKeysClientSide, ok := message.(*internal.RawKeysClientSide)
+	if !ok {
+		w.logger.Error(fmt.Sprintf("invalid data fetched from queue. Expected RawKeysClientSide. Got '%T'", message))
+		return nil
+	}
+
+	w.recorder.RecordRaw("/keys/cs", asTelemetryKeysClientSide.Payload, asTelemetryKeysClientSide.Metadata, nil)
+	return nil
+}
+
+func newTelemetryKeysClientSideWorkerFactory(name string, recorder *api.HTTPTelemetryRecorder, logger logging.LoggerInterface) WorkerFactory {
+	var i *int = common.IntRef(0)
+	return func() workerpool.Worker {
+		defer func() { *i++ }()
+		return &TelemetryKeysClientSideWorker{name: fmt.Sprintf("%s_%d", name, i), logger: logger, recorder: recorder}
+	}
+}
+
+// NewTelemetryKeysClientSideFlushTask creates a new flushing task
+func NewTelemetryKeysClientSideFlushTask(recorder *api.HTTPTelemetryRecorder, logger logging.LoggerInterface, period int, queueSize int, threads int) *DeferredRecordingTaskImpl {
+	return newDeferredFlushTask(logger, newTelemetryKeysClientSideWorkerFactory("telemetry-keys-client-side-worker", recorder, logger), period, queueSize, threads)
+}
+
+// Keys Server Side
+
+// TelemetryKeysServerSideWorker defines a component capable of recording mtk server side in raw form
+type TelemetryKeysServerSideWorker struct {
+	name     string
+	logger   logging.LoggerInterface
+	recorder *api.HTTPTelemetryRecorder
+}
+
+// Name returns the name of the worker
+func (w *TelemetryKeysServerSideWorker) Name() string { return w.name }
+
+// OnError is called whenever theres an error in the worker function
+func (w *TelemetryKeysServerSideWorker) OnError(e error) {}
+
+// Cleanup is called after the worker is shutdown
+func (w *TelemetryKeysServerSideWorker) Cleanup() error { return nil }
+
+// FailureTime specifies how long to wait when an errors occurs before executing again
+func (w *TelemetryKeysServerSideWorker) FailureTime() int64 { return 1 }
+
+// DoWork is called and passed a message fetched from the work queue
+func (w *TelemetryKeysServerSideWorker) DoWork(message interface{}) error {
+	asTelemetryKeysServerSide, ok := message.(*internal.RawKeysServerSide)
+	if !ok {
+		w.logger.Error(fmt.Sprintf("invalid data fetched from queue. Expected RawKeysServerSide. Got '%T'", message))
+		return nil
+	}
+
+	w.recorder.RecordRaw("/keys/ss", asTelemetryKeysServerSide.Payload, asTelemetryKeysServerSide.Metadata, nil)
+	return nil
+}
+
+func newTelemetryKeysServerSideWorkerWorkerFactory(name string, recorder *api.HTTPTelemetryRecorder, logger logging.LoggerInterface) WorkerFactory {
+	var i *int = common.IntRef(0)
+	return func() workerpool.Worker {
+		defer func() { *i++ }()
+		return &TelemetryKeysServerSideWorker{name: fmt.Sprintf("%s_%d", name, i), logger: logger, recorder: recorder}
+	}
+}
+
+// NewTelemetryKeysServerSideFlushTask creates a new flushing task
+func NewTelemetryKeysServerSideFlushTask(recorder *api.HTTPTelemetryRecorder, logger logging.LoggerInterface, period int, queueSize int, threads int) *DeferredRecordingTaskImpl {
+	return newDeferredFlushTask(logger, newTelemetryKeysServerSideWorkerWorkerFactory("telemetry-keys-server-side-worker", recorder, logger), period, queueSize, threads)
+}
