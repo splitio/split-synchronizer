fedac93dcf8d799494916ba4b165cce5c9fecf0d
Matias Melograno <matias.melograno@split.io>
2023-03-09 18:27:56 -0300

    updated to latest in commons

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,4 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-const CommitVersion = "b9c23c9"
+const CommitVersion = "c0e9499"
