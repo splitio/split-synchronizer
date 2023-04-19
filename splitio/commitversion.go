f9bccb15190e35b466d7d527ae7728160b910174
Martin Redolatti <martin.redolatti@split.io>
2023-04-19 17:53:02 -0300

    prepare for release

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,8 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-<<<<<<< HEAD
-const CommitVersion = "b9c23c9"
-=======
-const CommitVersion = "c0e9499"
->>>>>>> origin/feat/proxy_tls_support
+const CommitVersion = "3cb8482"
