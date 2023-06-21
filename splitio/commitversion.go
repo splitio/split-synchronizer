373bd7ef57bd1fcf18d65d5456b1223670f1fab7
Matias Melograno <matias.melograno@split.io>
2023-06-21 14:59:30 -0300

    updated docker images to go1.20.5, alpine to 3.18 and gin to v1.9.1 for fixing several vulnerabilities

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,4 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-const CommitVersion = "c565f10"
+const CommitVersion = "5a9c28d"
