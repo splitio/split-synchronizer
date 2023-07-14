bc091d84b407525181bca499275270ebbb943a2f
Martin Redolatti <martin.redolatti@split.io>
2023-07-14 14:49:41 -0300

    fix log message when fetching imps/events/uks fails

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,4 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-const CommitVersion = "5d527e7"
+const CommitVersion = "cf92e16"
