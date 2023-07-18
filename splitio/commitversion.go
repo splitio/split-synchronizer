da63b9fd93c751512275728894b3625422c7357b
Matias Melograno <matias.melograno@split.io>
2023-07-18 14:59:22 -0300

    prepare release for 5.4.0 with iffu

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,4 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-const CommitVersion = "b9e384d"
+const CommitVersion = "1049e07"
