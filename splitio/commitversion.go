30983e0621f3805a007c0baf9f8d14e0c1ce6bd2
Lucas Echeverz <lucas.echeverz@split.io>
2023-10-24 13:13:57 -0300

    no message

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,4 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-const CommitVersion = "9ed4ca5"
+const CommitVersion = "03a9105"
