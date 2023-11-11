12fd3b005df5922f8fea929e09106f44522f3991
Martin Redolatti <martin.redolatti@split.io>
2023-11-11 19:45:34 -0300

    more uts, enable integration

--- splitio/proxy/flagsets/flagsets.go
+++ splitio/proxy/flagsets/flagsets.go
@@ -1,5 +1,7 @@
 package flagsets
 
+import "golang.org/x/exp/slices"
+
 type FlagSetMatcher struct {
 	strict bool
 	sets   map[string]struct{}
@@ -18,18 +20,29 @@ func NewMatcher(strict bool, fetched []string) FlagSetMatcher {
 	return out
 }
 
+// Sort, Dedupe & Filter input flagsets. returns sanitized list and a boolean indicating whether a sort was necessary
 func (f *FlagSetMatcher) Sanitize(input []string) []string {
-	if !f.strict || len(input) == 0 {
+	if len(input) == 0 {
 		return input
 	}
 
-	for idx := range input {
-		if _, ok := f.sets[input[idx]]; !ok {
+	seen := map[string]struct{}{}
+	for idx := 0; idx < len(input); idx++ { // cant use range because we're srhinking the slice inside the loop
+		item := input[idx]
+		if (f.strict && !setContains(f.sets, item)) || setContains(seen, item) {
 			if idx+1 < len(input) {
 				input[idx] = input[len(input)-1]
 			}
 			input = input[:len(input)-1]
 		}
+		seen[item] = struct{}{}
 	}
+
+	slices.Sort(input)
 	return input
+}
+
+func setContains(set map[string]struct{}, item string) bool {
+	_, ok := set[item]
+	return ok
 }
