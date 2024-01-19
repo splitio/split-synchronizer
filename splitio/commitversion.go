772077b91eb4d887d45c6876611cfc719851f595
Martin Redolatti <martin.redolatti@split.io>
2024-01-19 11:47:20 -0300

    allow fips enforcement when building

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,4 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-const CommitVersion = "5ed4245"
+const CommitVersion = "0f2619f"
