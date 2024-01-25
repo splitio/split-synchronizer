396917474394555c2f4228ca644fa8eb8c558b81
Martin Redolatti <martin.redolatti@split.io>
2024-01-25 11:44:48 -0300

    fix asset copyting in linux build env

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,4 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-const CommitVersion = "ec42bee"
+const CommitVersion = "27e4829"
