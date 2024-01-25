27e4829adc4042bbf769c367fb09c85fb5f480b9
Martin Redolatti <martin.redolatti@split.io>
2024-01-25 11:36:53 -0300

    more asset folder cleanup

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,4 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-const CommitVersion = "d5dc3d0"
+const CommitVersion = "ec42bee"
