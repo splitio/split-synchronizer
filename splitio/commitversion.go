a2bdfc6986c774675b7eedcc9c3459c2f6711538
Martin Redolatti <martin.redolatti@split.io>
2024-02-21 20:47:41 -0300

    .

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,4 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-const CommitVersion = "9bb7416"
+const CommitVersion = "e96f5c1"
