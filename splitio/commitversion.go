424f3825b6d60db216762faf3cd5cb6f9a3f1ce3
Martin Redolatti <martin.redolatti@split.io>
2024-04-03 13:15:28 -0300

    forward single server name when connecting to multiple redis hosts in cluster or sentinel modes

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,4 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-const CommitVersion = "1ffbd68"
+const CommitVersion = "94c6f52"
