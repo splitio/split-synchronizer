ea875c940b61eebfb919350e7ed37656e62a205a
Lucas Echeverz <lucas.echeverz@split.io>
2024-05-06 15:26:13 -0300

    Changing specVersion strings

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,4 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-const CommitVersion = "00c9951"
+const CommitVersion = "a1d87c7"
