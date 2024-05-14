7e48cc24353ad4e232f5b95413e99908c407cf16
Matias Melograno <matias.melograno@split.io>
2024-05-14 14:14:44 -0300

    prepare 5.8.0

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,4 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-const CommitVersion = "e546780"
+const CommitVersion = "ae5a5ac"
