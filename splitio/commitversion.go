54d67fda208e8c311fe9424bbb3e25dc79fcbcc8
Mauro Antonio Sanz <mauro.sanz@split.io>
2024-09-13 16:18:19 -0300

    fixing healthcheck stats

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,4 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-const CommitVersion = "ae5a5ac"
+const CommitVersion = "8d090ac"
