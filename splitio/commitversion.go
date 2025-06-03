a42cf7ab1de9c45921983712aa4507204917ac1e
Matias Melograno <matias.melograno@split.io>
2025-06-03 16:28:31 -0300

    added json files

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,4 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-const CommitVersion = "c6be87f"
+const CommitVersion = "706f8c7"
