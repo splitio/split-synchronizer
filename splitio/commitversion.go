029a2b0325097ae41001a72a7815e6b73c531791
Matias Melograno <matias.melograno@split.io>
2025-06-04 15:46:39 -0300

    # This is a combination of 4 commits. # This is the 1st commit message:
    added endpoints
    
    # This is the commit message #2:
    
    Dashboard - fetching data and displaying in table
    
    # This is the commit message #3:
    
    Add kill and discard action buttons
    
    # This is the commit message #4:
    
    edit flag from slide out modal

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,4 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-const CommitVersion = "a42cf7a"
+const CommitVersion = "d219c19"
