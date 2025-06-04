fc29bac31ac050831b1ff3ee9acba1daddb36702
Orlando Augusto Petruccelli <augusto.petruccelli@split.io>
2025-06-04 15:02:01 -0300

    Dashboard - fetching data and displaying in table

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+// import '@blueprintjs/core/lib/css/blueprint.css';
+// import '@blueprintjs/datetime/lib/css/blueprint-datetime.css';
+// import '@harness/uicore/dist/index.css';
+
+const container = document.getElementById('root')
+if (!container) throw new Error('Root container missing in index.html')
+
+const root = createRoot(container)
+
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>
 )
