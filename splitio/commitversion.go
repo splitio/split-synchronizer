514a95c2c15ee79e9b6668df3c327eb47cae35c5
Nadia Mayor <nadia.mayor@split.io>
2025-11-04 13:50:51 -0300

    Updated status

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,4 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-const CommitVersion = "f8ab000"
+const CommitVersion = "1fbc498"
