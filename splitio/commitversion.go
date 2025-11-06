7ab27861b33ec4c72000021a9b70bec88510ea57
Nadia Mayor <nadia.mayor@split.io>
2025-11-06 16:16:05 -0300

    Updated cached rule-based segments

--- splitio/commitversion.go
+++ splitio/commitversion.go
@@ -5,4 +5,4 @@ This file is created automatically, please do not edit
 */
 
 // CommitVersion is the version of the last commit previous to release
-const CommitVersion = "8fd26d7"
+const CommitVersion = "88290d5"
