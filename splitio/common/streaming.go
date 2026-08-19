b1624764704c5d7aec1784c52c235e99f86638b4
alejovazquezsplit <alejo.vazquez@harness.io>
2026-08-19 18:13:28 -0300

    logging just http1

--- splitio/common/streaming.go
+++ splitio/common/streaming.go
@@ -2,18 +2,10 @@ package common
 
 import "github.com/splitio/go-toolkit/v5/logging"
 
-// LogStreamingProtocol emits an info line describing which HTTP protocol the
-// streaming (SSE) connection is configured to use. It reflects the configured
-// intent (the streaming-force-http1 flag), not the protocol negotiated on the
-// wire. When streaming is disabled there is no SSE connection, so nothing is
-// logged.
-func LogStreamingProtocol(logger logging.LoggerInterface, streamingEnabled bool, forceHTTP1 bool) {
-	if !streamingEnabled {
-		return
+// LogStreamingForceHTTP1 logs an info line when the streaming (SSE) connection
+// is pinned to HTTP/1.1 via the streaming-force-http1 setting.
+func LogStreamingForceHTTP1(logger logging.LoggerInterface, streamingEnabled bool, forceHTTP1 bool) {
+	if streamingEnabled && forceHTTP1 {
+		logger.Info("Streaming (SSE) connection forced to HTTP/1.1")
 	}
-	if forceHTTP1 {
-		logger.Info("Streaming (SSE) connection configured to use HTTP/1.1")
-		return
-	}
-	logger.Info("Streaming (SSE) connection configured to use HTTP/2")
 }
--- splitio/proxy/initialization.go
+++ splitio/proxy/initialization.go
@@ -82,7 +82,7 @@ func Start(logger logging.LoggerInterface, cfg *pconf.Main) error {
 	advanced.FlagSetsFilter = cfg.FlagSetsFilter
 	advanced.AuthSpecVersion = cfg.FlagSpecVersion
 	advanced.FlagsSpecVersion = cfg.FlagSpecVersion
-	common.LogStreamingProtocol(logger, advanced.StreamingEnabled, advanced.StreamingForceHTTP1)
+	common.LogStreamingForceHTTP1(logger, advanced.StreamingEnabled, advanced.StreamingForceHTTP1)
 	metadata := util.GetMetadata(cfg.IPAddressEnabled, true)
 
 	// FlagSetsFilter
